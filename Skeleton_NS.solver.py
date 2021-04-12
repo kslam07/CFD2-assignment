b7dc8a19ce472d24e3c170e27b9cfb16a4300e5f
Sam Lam <trykslam@gmail.com>
2021-04-12 19:42:55 +0200

    Fix streamfunction, x,y = (0,0) as reference point

--- Skeleton_NS.solver.py
+++ Skeleton_NS.solver.py
@@ -572,13 +572,14 @@ def plot_contour(N, x, u_org, p_org, h):
 def plot_streamfunction(N, u, x):
     X, Y = np.meshgrid(x, x)
 
-    phi = sparse.linalg.lsqr(tE10, tu)[0].reshape(N + 1, N + 1)
+    phi = sparse.linalg.lsqr(tE10, Ht11 @ u)[0].reshape(N + 1, N + 1)
+    phi = phi - phi[0,0]
     # -1.5e-3, -1e-3, -5e-4, -2.5e-4, -1e-4, -5e-5, -1e-5, -1e-6,
     lvls = [-1.5e-3, -1e-3, -5e-4, -2.5e-4, -1e-4, -5e-5, -1e-5, -1e-6, 0.0, 1e-10, 1e-5, 1e-4, 1e-2, 3e-2, 5e-2, 7e-2,
             9e-2, 0.1, 0.11, 0.115, 0.1175]
     # lvls = [0.115, 0.1175]
     fig, ax = plt.subplots(1, 1, dpi=150)
-    cs = ax.contour(X, Y, phi[::-1], levels=lvls)
+    cs = ax.contour(X, Y, phi, levels=lvls)
     ax.clabel(cs, inline=1, fontsize=12)
     ax.set_xlabel("x [-]")
     ax.set_ylabel("y [-]")
